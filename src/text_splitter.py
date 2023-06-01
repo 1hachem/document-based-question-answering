20e2df46b84d11295af753232c4b6ea1f8137519
1hachem <hachem.betrouni@g.enp.edu.dz>
2023-06-01 13:09:20 +0100

    add paragraph splitter

--- src/text_splitter.py
+++ src/text_splitter.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 from langchain.text_splitter import TokenTextSplitter
 from sentence_splitter import SentenceSplitter as SentenceSplitter_
 
+
 class Splitter(ABC):
     @abstractmethod
     def split(self, text: str) -> list["str"]:
@@ -17,8 +18,10 @@ class TokenSplitter(Splitter):
         self.text_splitter = TokenTextSplitter(
             chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap
         )
+
     def split(self, text: str) -> list[str]:
         return self.text_splitter.split_text(text)
+
 
 class SentenceSplitter(Splitter):
     def __init__(self) -> None:
@@ -28,11 +31,46 @@ class SentenceSplitter(Splitter):
     def split(self, text: str) -> list[str]:
         output = self.text_splitter.split(text)
         # drop empty string from list
-        output = [x for x in output if x!=""]
+        output = [x for x in output if x != ""]
         return output
 
+
+class ParagarphSplitter(Splitter):
+    def __init__(self) -> None:
+        super().__init__()
+        self.text_splitter = SentenceSplitter_(language="en")
+
+    def split(self, text: str) -> list[str]:
+        # split to sentences
+        output = self.text_splitter.split(text)
+        output = [x for x in output if x != ""]  # drop empty strings
+        output = self.join_sentences(output)
+        return output
+
+    def join_sentences(self, sentences: list[str]) -> list[str]:
+        """
+        takes a list of sentence and returns a list of paragraphs
+        """
+        result = []
+        current_list = []
+
+        for sentence in sentences:
+            current_list.append(sentence)
+
+            if sentence.endswith("."):
+                result.append(current_list)
+                current_list = []
+
+        if current_list:
+            result.append(current_list)
+
+        result = ["".join(para) for para in result]
+        return result
+
+
 if __name__ == "__main__":
-    output = SentenceSplitter().split("""
+    output = ParagarphSplitter().split(
+        """
     Once upon a time, in the small town of Willowbrook, there lived a curious young girl named Emily. 
     With her bright eyes and an insatiable thirst for adventure, she was always seeking new experiences and discoveries.
     One sunny morning, Emily ventured into the dense forest that surrounded her town. 
@@ -46,5 +84,6 @@ if __name__ == "__main__":
     In the center stood a majestic waterfall, 
     cascading down from the rocks above into a crystal-clear pool below. The sight was breathtaking, 
     and Emily couldn't help but be in awe of its beauty.
-    """)
+    """
+    )
     print(output)
