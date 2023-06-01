4f0d99097074cce22901b012b293a710201f3d59
1hachem <hachem.betrouni@g.enp.edu.dz>
2023-06-01 13:08:59 +0100

    add bert

--- src/embedding.py
+++ src/embedding.py
@@ -1,6 +1,10 @@
 from abc import ABC, abstractmethod
 
+import torch
 from sentence_transformers import SentenceTransformer
+from transformers import AutoTokenizer, BertForQuestionAnswering
+
+from src.similarity_measure import CosineSimilarity
 
 
 class Embedder(ABC):
@@ -12,13 +16,39 @@ class Embedder(ABC):
 class MiniLM(Embedder):
     def __init__(self) -> None:
         super().__init__()
-        self.model = SentenceTransformer("all-MiniLM-L6-v2")
+        self.model = SentenceTransformer("all-MiniLM-L6-v2", device="cpu")
 
     def embed(self, texts: list[str]) -> list[list[float]]:
         embeddings = self.model.encode(texts)
         return embeddings.tolist()
 
 
+class Bert(Embedder):
+    def __init__(self) -> None:
+        super().__init__()
+        self.tokenizer = AutoTokenizer.from_pretrained("deepset/bert-base-cased-squad2")
+        self.model = BertForQuestionAnswering.from_pretrained(
+            "deepset/bert-base-cased-squad2"
+        )
+
+    def embed(self, texts: list[str]) -> list[list[float]]:
+        inputs = [self.tokenizer(text, return_tensors="pt") for text in texts]
+        with torch.no_grad():
+            outputs = [
+                self.model(**input, output_hidden_states=True) for input in inputs
+            ]
+            outputs = [
+                output.hidden_states[-1].mean(dim=1).flatten().tolist()
+                for output in outputs
+            ]
+
+        return outputs
+
+
 if __name__ == "__main__":
-    output = MiniLM().embed(["Hello, my dog is cute", "how to kill a bird"])
-    print(output.shape)
+    # output = MiniLM().embed(["Hello, my dog is cute", "how to kill a bird"])
+    # print(output.shape)
+
+    bert = Bert()
+    output = bert.embed(["what is my name", "my name is not bob"])
+    print(CosineSimilarity().measure(output[0], output[1]))
