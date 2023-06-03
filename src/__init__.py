2d1016dab284f82ef81917ac1d5f14ffe723ae31
1hachem <hachem.betrouni@g.enp.edu.dz>
2023-06-03 14:36:48 +0100

    list everything here

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,9 @@
 from src.embedding import Bert, MiniLM
 from src.lm import GPT2, GPTJ, LlamaQ4, VicunaQ8
 from src.similarity_measure import CosineSimilarity
+from src.text_splitter import SentenceSplitter, TokenSplitter, ParagarphSplitter
 
-llms = [GPT2, LlamaQ4, VicunaQ8, GPTJ]
+splitters = [SentenceSplitter, TokenSplitter, ParagarphSplitter]
 embedding_models = [Bert, MiniLM]
 measures = [CosineSimilarity]
+llms = [GPT2, LlamaQ4, VicunaQ8, GPTJ]
