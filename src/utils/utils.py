aad0f7a14f42942661c453eb9cb1ceb475a42102
1hachem <hachem.betrouni@g.enp.edu.dz>
2023-06-08 15:10:20 +0100

    update utils

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,4 +1,5 @@
 import pickle
+import json
 
 
 def dump_pickle(somthing: any, path: str) -> None:
@@ -9,3 +10,15 @@ def dump_pickle(somthing: any, path: str) -> None:
 def read_pickle(path) -> any:
     with open(path, "rb") as f:
         return pickle.load(f)
+
+
+# Open the JSON file for reading
+def read_json(path: str):
+    with open(path, "r") as file:
+        data = json.load(file)
+    return data
+
+
+def save_json(data: dict, path: str):
+    with open(path, "w") as file:
+        json.dump(data, file, indent=4)
