3159fb41733a22c0e68451d5927c27bb54eb27bc
1hachem <hachem.betrouni@g.enp.edu.dz>
2023-06-08 15:10:09 +0100

    update init

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,9 @@
 from src.embedding import Bert, MiniLM
-from src.lm import GPT2, GPTJ, LlamaQ4
+from src.lm import GPT2, GPTJ, Llama, Falcon
 from src.similarity_measure import CosineSimilarity
-from src.text_splitter import (ParagarphSplitter, SentenceSplitter,
-                               TokenSplitter)
+from src.text_splitter import ParagarphSplitter, SentenceSplitter, TokenSplitter
 
-splitters = [SentenceSplitter, TokenSplitter, ParagarphSplitter]
+splitters = [SentenceSplitter, ParagarphSplitter]
 embedding_models = [Bert, MiniLM]
 measures = [CosineSimilarity]
-llms = [GPT2, LlamaQ4, GPTJ]
+llms = [GPT2, Falcon, GPTJ]
