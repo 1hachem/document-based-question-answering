53d834dbaa1c39a7400ccec4fc65570613340996
1hachem <hachem.betrouni@g.enp.edu.dz>
2023-06-11 16:29:47 +0100

    add script

---
+++ scripts/evaluate_benchmark.py
@@ -0,0 +1,13 @@
+import asyncio
+
+from src.benchmark import evaluate_inferences, parse_results
+from src.utils.utils import read_json, save_json
+
+PATH = "results/benchmarks/squad.json"
+SAVE_PATH = "results/summary/squad.json"
+
+if __name__ == "__main__":
+    indexes = read_json(PATH)
+    indexes = asyncio.run(evaluate_inferences(indexes))
+    table = parse_results(indexes)
+    save_json(table, SAVE_PATH)
