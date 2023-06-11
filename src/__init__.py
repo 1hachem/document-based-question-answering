810ac48748eabd6a05e657c93eb17fad58404bcf
1hachem <hachem.betrouni@g.enp.edu.dz>
2023-06-11 17:20:54 +0100

    update

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,10 @@
 from src.embedding import Bert, MiniLM
-from src.lm import GPT2, GPTJ, Falcon, Llama
+from src.lm import GPT2, Falcon
 from src.similarity_measure import CosineSimilarity
-from src.text_splitter import ParagarphSplitter, SentenceSplitter
+from src.text_splitter import SentenceSplitter, TokenSplitter
 from src.vicuna import Vicuna
 
-splitters = [SentenceSplitter, ParagarphSplitter]
+splitters = [SentenceSplitter, TokenSplitter]
 embedding_models = [Bert, MiniLM]
 measures = [CosineSimilarity]
-llms = [Falcon, Vicuna]
+llms = [Vicuna, Falcon]
