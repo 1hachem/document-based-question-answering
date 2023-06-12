41b630b90b421b1965cbd2f1a07829b404439dc4
1hachem <hachem.betrouni@g.enp.edu.dz>
2023-06-12 12:47:15 +0100

    add E5

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,10 @@
-from src.embedding import Bert, MiniLM
+from src.embedding import Bert, MiniLM, E5
 from src.lm import Falcon, Llama
 from src.similarity_measure import CosineSimilarity
 from src.text_splitter import SentenceSplitter
 from src.vicuna import Vicuna
 
 splitters = [SentenceSplitter]
-embedding_models = [Bert, MiniLM]
+embedding_models = [Bert, MiniLM, E5]
 measures = [CosineSimilarity]
 llms = [Vicuna, Falcon, Llama]
