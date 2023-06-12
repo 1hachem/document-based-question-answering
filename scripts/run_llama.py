1cbbfa97c727417347f74fcca3e7b4d85c1839c8
1hachem <hachem.betrouni@g.enp.edu.dz>
2023-06-12 10:45:21 +0100

    update llama test

--- scripts/run_llama.py
+++ scripts/run_llama.py
@@ -1,5 +1,9 @@
 from src.lm import Llama
 
-prompts = ["hello llama", "how is the weather in dreamland ?"]
+prompts = [
+    """question : What is the main reason consulting pharmacists are increasingly working directly with patients?
+    context : This trend may be gradually reversing as consultant pharmacists begin to work directly with patients, primarily because many elderly people are now taking numerous medications but continue to live outside of institutional settings.
+    answer :""",
+]
 llm = Llama()
 llm(prompts)
